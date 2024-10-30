6bb909c1f06ad5712dd09f4a3e6cd5308281eca7
TymoteuszBroda <Tymoteusz.91100@gmail.com>
2024-10-30 18:11:16 +0100

    Added Register component

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule, FormsModule, PasswordModule, ButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent{
+login: string | undefined;
+password: string | undefined;
 
+
+handleLogin()
+{
+  console.log(this.login);
+  console.log(this.password);
 }
+}
+
+
