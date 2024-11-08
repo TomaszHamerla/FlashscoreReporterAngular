d4944f3b14a8b7a550ac2c15bc23b1fc52704d42
TymoteuszB <Tymoteusz.91100@gmail.com>
2024-11-08 01:54:36 +0100

    Added basic registration component based on login component

--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule, FormsModule, PasswordModule, ButtonModule, FloatLabelModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+login: string | undefined;
+password: string | undefined;
+
+handleRegistration()
+{
+  console.log(this.login);
+  console.log(this.password);
+}
 
 }
+
+
