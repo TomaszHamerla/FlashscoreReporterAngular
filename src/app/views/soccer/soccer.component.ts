500a2aa583f2e1cbf614f43201193ab1f3a5e2c3
TymoteuszBroda <Tymoteusz.91100@gmail.com>
2024-11-27 11:25:15 +0100

    added soccer games data display in form of table

--- src/app/views/soccer/soccer.component.ts
+++ src/app/views/soccer/soccer.component.ts
@@ -1,28 +1,35 @@
 import {Component, OnInit} from '@angular/core';
 import {GameWithDetailsDto} from "../../service/models/game-with-details-dto";
 import {GameService} from "../../service/services/game.service";
+import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+
 
 @Component({
   selector: 'app-soccer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TableModule],
   templateUrl: './soccer.component.html',
-  styleUrl: './soccer.component.scss'
+  styleUrl: './soccer.component.scss',
 })
 export class SoccerComponent implements OnInit {
+  cols = [
+    { field: 'awayTeamName', header: 'Gość' },
+    { field: 'awayTeamLogo' },
+    { field: 'homeTeamLogo' },
+    { field: 'homeTeamName', header: 'Gospodarz' },
+    { field: 'gameResult', header: 'Wynik' },
+  ];
   gamesData!: GameWithDetailsDto[];
 
-  constructor(
-    private gameService: GameService
-  ) {
-  }
+  constructor(private gameService: GameService) {}
 
   ngOnInit(): void {
     this.gameService.getGames().subscribe({
-      next: val => {
+      next: (val) => {
         this.gamesData = val;
-        console.log(val)
-      }
-    })
+        console.log(val);
+      },
+    });
   }
 }
